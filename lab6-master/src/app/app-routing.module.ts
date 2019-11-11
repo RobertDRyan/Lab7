1472ccb9fd08f721d479bf01dfe47a817c559a20
RobertDRyan <G00332617@gmit.ie>
2019-11-11 11:51:58 +0000

    Lab9 Finished Edit working

--- lab6-master/src/app/app-routing.module.ts
+++ lab6-master/src/app/app-routing.module.ts
@@ -13,10 +13,11 @@ const routes: Routes = [
     component: CreateComponent
   },
   {
-  path: 'edit',
-    component:EditComponent
-  }
-];
+    path:'edit/:id',
+    component: EditComponent
+    }
+    ];
+    
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
